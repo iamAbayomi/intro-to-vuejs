669aaa9fd9c934897bb530ee9077f39747dfb461
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-06 05:39:41 +0100

    Created Employee-form

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -8,27 +8,23 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>Richard Hendricks</td>
-                    <td>richard@piedpiper.com</td>
+                <tr v-for = "employee in employees"  :key = "employee.id" >
+                    <td>{{employee.name}}</td>
+                    <td>{{employee.email}}</td>
                 </tr>
-                <tr>
-                    <td>Bertram Gilfoyle</td>
-                    <td>gilfoyle@piedpiper.com</td>
-                </tr>
-                <tr>
-                    <td>Dinesh Chugtai</td>
-                    <td>dinesh@piedpiper.com</td>
-                </tr>    
             </tbody>
         </table>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'employee-table'
-}
+    export default {
+        name: 'employee-table',
+        props: {
+            employees: Array,
+        },
+    }
+
 </script>
 
 
