72af3828210863a96cd783c4d53544b3a2b502f7
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-06 08:58:19 +0100

    Added edit and delete button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
     <div id="app" class="small-container">
       <h1>Employees</h1>
-      <employee-form />
-      <employee-table :employees = "employees" />
+      <employee-form @add:employee = "addEmployee" />
+      <employee-table 
+        :employees = "employees"
+        @delete:employee = "deleteEmployee"
+        @edit:employee = "editEmployee"
+         />
     </div>
 
 </template>
@@ -40,6 +44,32 @@
          ],
        }
      },
+     methods:{
+       addEmployee(employee){
+            const lastId =
+              this.employees.length > 0
+                ? this.employees[this.employees.length-1].id
+                : 0;
+            const id = lastId + 1;
+            const newEmployee = { ...employee, id };
+
+            this.employees = [ ...this.employees, newEmployee ];
+       },
+
+      deleteEmployee(id){
+        this.employees = this.employees.filter(
+          employee => employee.id !== id
+        )
+        },
+
+      editEmployee( id, updatedEmployee){
+        this.employee = this.employee.map(employee=>
+            employee.id === id ? updatedEmployee : employee
+        )
+
+      }  
+
+     }
   }
 </script>
 
